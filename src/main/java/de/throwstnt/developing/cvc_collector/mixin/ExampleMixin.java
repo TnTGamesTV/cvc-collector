26e25ece8cb2f8169cdcf1bd8e0daee73a622545
Finn Tegeler <developing@finn-tegeler.de>
2021-10-06 11:02:32 +0200

    Added core components for addon

---
+++ src/main/java/de/throwstnt/developing/cvc_collector/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package de.throwstnt.developing.cvc_collector.mixin;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class ExampleMixin {
+
+  @Inject(method = "runTick", at = @At("HEAD"))
+  private void injectRunTick(CallbackInfo ci) {
+  }
+
+}
