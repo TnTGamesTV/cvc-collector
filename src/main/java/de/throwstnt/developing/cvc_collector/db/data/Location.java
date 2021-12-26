ab3b6c81a11e67100721c79c4130a4eb677e2fd8
Finn Tegeler <developing@finn-tegeler.de>
2021-12-26 13:20:43 +0100

    Added serializers and data types for storing collected data

--- src/main/java/de/throwstnt/developing/cvc_collector/db/data/Location.java
+++ src/main/java/de/throwstnt/developing/cvc_collector/db/data/Location.java
@@ -1,34 +1,40 @@
 package de.throwstnt.developing.cvc_collector.db.data;
 
+import de.throwstnt.developing.cvc_collector.output.data.UnlinkedLocation;
+
 public class Location {
 
-  final String map;
-  
-  final double x;
-  final double y;
-  final double z;
-  
-  public Location(String map, double x, double y, double z) {
-    this.map = map;
-    
-    this.x = x;
-    this.y = y;
-    this.z = z;
-  }
-  
-  public String getMap() {
-    return map;
-  }
+    final String map;
 
-  public double getX() {
-    return x;
-  }
+    final double x;
+    final double y;
+    final double z;
 
-  public double getY() {
-    return y;
-  }
+    public Location(String map, double x, double y, double z) {
+        this.map = map;
 
-  public double getZ() {
-    return z;
-  }
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public String getMap() {
+        return map;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public UnlinkedLocation toUnlinkedLocation() {
+        return new UnlinkedLocation((int) x, (int) y, (int) z);
+    }
 }
